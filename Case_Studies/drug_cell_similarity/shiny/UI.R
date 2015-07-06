56397267e20f87013c0e7daeb9ead50c2291323c
Adrian She <adrian.she@alumni.ubc.ca>
2015-07-06 15:56:33 -0400

    Make drug similarity app more responsive to inputs

--- Case_Studies/drug_cell_similarity/shiny/UI.R
+++ Case_Studies/drug_cell_similarity/shiny/UI.R
@@ -1,25 +1,32 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("Drug Similarity Search"),
-  sidebarLayout(
-    sidebarPanel(
-      p("Search by:"),
-      radioButtons("optn", label = "", choice = list("SMILES String" = "smiles", "Chemical Name" = "name")),
-      uiOutput("prompt"),
-      checkboxGroupInput("studies", label = h4("Studies"), list("CCLE", "CGP", "GRAY", "GNE", "GSK", "CTRP", "NCI60", "all studies")),
-      sliderInput("cutoff", label = h4("Cutoff"), min = 0, max = 100, value = 50),
-      checkboxInput("display", label = "Display SMILES and InChIKeys of Drugs", value = FALSE),
-      checkboxInput("chembl", label = "Display links to ChEMBL", value = FALSE),
-      submitButton("Submit!")),
-    mainPanel(
-      p("Search for similar drugs in large-scale pharmacogenomic studies to a given drug according using the Tanimoto algorithm."),
-      p("Current studies include CCLE, CGP, GRAY, GNE, GSK, CTRP, and NCI60"),
-      textOutput("params"),
-      h3("Results"),
-      textOutput("res"),
-      dataTableOutput("similarDrugs")
-    )
-    )
-))
+shinyUI(
+  
+  fluidPage(
+      titlePanel("Drug Similarity Search"),
+      sidebarLayout(
+          sidebarPanel(
+              selectInput("optn", label = "Search by:", choice = c("SMILES String", "Chemical Name")),
+              uiOutput("prompt"),
+              checkboxGroupInput("studies", label = h4("Studies"), list("CCLE", "CGP", "GRAY", "GNE", "GSK", "CTRP", "NCI60")),
+              sliderInput("cutoff", label = h4("Cutoff in %"), min = 0, max = 100, value = 50),
+              checkboxInput("row", label = "Display one row per drug", value = FALSE),
+              checkboxInput("smiles", label = "Display SMILES", value = FALSE),
+              checkboxInput("inchikey", label = "Display InChIKey", value = FALSE),
+              checkboxInput("chembl", label = "Display links to ChEMBL", value = FALSE),
+              actionButton("submitButton", "Submit!")
+              ),
+          mainPanel(
+              p("Search for similar drugs in large-scale pharmacogenomic studies to a given drug according using the Tanimoto algorithm."),
+              p("Current studies include CCLE, CGP, GRAY, GNE, GSK, CTRP, and NCI60."),
+              p("By default, an input drug will be searched against drugs in all studies."),
+              textOutput("params"),
+              h3("Results"),
+              textOutput("record"),
+              textOutput("res"),
+              dataTableOutput("similarDrugs")
+        )
+      )
+      )
+  )
 
--- Case_Studies/drug_cell_similarity/shiny/getObj.R
+++ Case_Studies/drug_cell_similarity/shiny/getObj.R
@@ -1,15 +1,26 @@
 library(RMySQL)
 library(rcdk)
+source("chemicalTransUtils.R")
+
+## Script to initialize application objects by
+## getting drug curation, drug fingerprints, and
+## chembl ids
 
 ## Get table for drug similarity application
 con <- dbConnect(MySQL(), dbname = "PharmacoDB", hostname = "localhost", username = "root", password = "")
-frame <- dbGetQuery(con, "SELECT * FROM drug_similiarity_app_view")
+drugs <- dbGetQuery(con, "SELECT * FROM  drug_similarity_app")
 dbDisconnect(con)
 
 ## Remove na values for smiles to get fingerprints
-frame <- frame[!is.na(frame$smiles), ]
-target.mols <- parse.smiles(as.character(frame[,"smiles"]))
+drugs <- drugs[!is.na(drugs$smiles), ]
+target.mols <- parse.smiles(as.character(drugs[,"smiles"]))
 target.fps <- lapply(target.mols, get.fingerprint, type="extended")
+drugs$fps <- target.fps
+save(drugs, file = "drug_dat_with_fps.RData")
 
-save(frame, file = "data_obj.RData")
-save(target.fps, file = "fingerprints.RData")
+## Get CHEMBL IDS
+chembls <- lapply(unique(drugs$drug_id), getChEMBLfromName)
+names(chembls) <- unique(drugs$drug_id)
+chembls <- Filter(function (x) (length(x) > 0 && !is.na(x)), chembls) ## Filter ChEMBL ids which were not found
+save(chembls, file = "chembls.RData")
+
