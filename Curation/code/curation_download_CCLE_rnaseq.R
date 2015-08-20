5a1f1533777fdcdf256c2ce776fab57c8d8d2956
Adrian She <adrian.she@alumni.ubc.ca>
2015-08-20 18:42:40 -0400

    Fix merge conflict

--- Curation/code/curation_download_CCLE_rnaseq.R
+++ Curation/code/curation_download_CCLE_rnaseq.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #### R Script to parse CCLE RNA-Seq Manifest File
 #### Manifest file cannot be downloaded automatically. It must be downloaded
 #### manually from https://browser.cghub.ucsc.edu/search/?study=("Other_Sequencing_Multiisolate)
@@ -6,6 +5,17 @@
 #### to cart and then downloading the manifest from that.
 
 library(XML)
+
+=======
+
+## path to files
+path.data <- file.path("data", "CCLE_rnaseq")
+path.cell <- file.path(path.data, "celline")
+## create directories
+if(!file.exists(path.data)) { dir.create(path.data, showWarnings=TRUE, recursive=TRUE) }
+if(!file.exists(path.cell)) { dir.create(path.cell, showWarnings=TRUE, recursive=TRUE) }
+
+## So the manifest of all ccle rnaseq cell lines should get download from CGhub to path.cell and then parse the xml file and create a csv file named cell_line_annotation_CCLE_rnaseq.csv as output
 
 ## x: name of XML file
 parseDoc <- function(x) {
@@ -15,7 +25,7 @@ parseDoc <- function(x) {
 	
 	## Obtain root of xml tree
 	top <- xmlRoot(doc)
-		
+    
 	## Parse results for each result
 	frames <- lapply(2:(length(top) - 1), function (x) {
 		val <- xmlToList(top[[x]])
@@ -32,31 +42,20 @@ parseDoc <- function(x) {
 }
 
 ## Assume in dir where all manifest files present
-if (!file.exists("manifest.xml")) {
+if (!file.exists(file.path(path.cell, "manifest.xml"))) {
 	stop("The manifest for CCLE RNA-Seq does not exist! Please download it from CGHub.")
 }
 
 ## parse file into a df
-frame <- parseDoc("manifest.xml")
+frame <- parseDoc(file.path(path.cell, "manifest.xml"))
 
 ## Create column cells with cell line name for each file
 cells <- sapply(as.vector(t(frame["file_name"])), function (x) {
-		str <- sub("\\.[0-9]\\.bam", "", x) ## remove end of file name
-		str <- sub("\\w*\\.", "", str) ## remove front of filename
-	 })
+    str <- sub("\\.[0-9]\\.bam", "", x) ## remove end of file name
+    str <- sub("\\w*\\.", "", str) ## remove front of filename
+})
 
 ## Construct and write final frame
 frame <- data.frame(frame, "cells" = cells, "tissueid" = NA)
-write.csv(frame, file = "ccle_rnaseq_curation.csv")
-=======
+write.csv(frame, file = "cell_line_annotation_CCLE_rnaseq.csv")
 
-## path to files
-path.data <- file.path("data", "CCLE_rnaseq")
-path.cell <- file.path(path.data, "celline")
-## create directories
-if(!file.exists(path.data)) { dir.create(path.data, showWarnings=TRUE, recursive=TRUE) }
-if(!file.exists(path.cell)) { dir.create(path.cell, showWarnings=TRUE, recursive=TRUE) }
-
-### Adrian please put your code to extract cell line name from the manifest here
-## So the manifest of all ccle rnaseq cell lines should get download from CGhub to path.cell and then parse the html file and create a csv file named cell_line_annotation_CCLE_rnaseq.csv as output
->>>>>>> 418e467c61132c0415d1d941145733a31cae7061
